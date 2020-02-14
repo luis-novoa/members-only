72b447f42f2e9e5896b1aacbee06de2a09c14924
Luis Novoa <luis.matte.novoa@gmail.com>
2020-02-14 18:13:49 -0400

    Fully integrate the website

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
     @user = User.find_by(email: params[:session][:email])
     if @user.authenticate(params[:session][:password])
       log_in(@user)
-      redirect_to root_url
+      redirect_to posts_path
     end
   end
 
