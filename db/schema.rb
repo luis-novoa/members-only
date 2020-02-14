2cfb87b18561c6c54c1772d8c21e24a566a3066e
Luis Novoa <luis.matte.novoa@gmail.com>
2020-02-14 12:11:08 -0400

    Add section controller and log in page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_14_152351) do
+ActiveRecord::Schema.define(version: 2020_02_14_160503) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2020_02_14_152351) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "remember_token"
   end
 
 end
