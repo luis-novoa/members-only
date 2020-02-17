27a291493f88f031c3b934620516267b6fafd5ca
davidauza.engineer <davidauza01@gmail.com>
2020-02-17 15:05:51 -0500

    Add Sign Up feature

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,11 @@ class User < ApplicationRecord
   before_create :assign_token
   has_secure_password
   has_many :posts
+  EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
+  validates :name, presence: true, length: { minimum: 4, maximum: 50 }
+  validates :email, presence: true, uniqueness: true, length: { maximum: 50 },
+                    format: { with: EMAIL_REGEX }
+  validates :password, presence: true, length: { minimum: 6, maximum: 20 }
 
   def assign_token
     self.remember_digest = User.encrypt(User.new_token)
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  get 'users/new'
+  get 'users/create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+  get '/signup', to: 'users#new'
   root "sessions#new"
   resources :sessions, only: [:new, :create, :destroy]
   resources :posts, only: [:new, :create, :index]
+  resources :users, only: [:new, :create, :index]
 end
